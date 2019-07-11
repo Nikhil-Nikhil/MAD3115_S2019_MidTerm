e693d8330299e175056688d686c1a1e89a1360aa
nikhil nikhil <nikhilthakur050@gmail.com>
2019-07-11 19:30:02 -0400

    Protocol IDisplay created

---
+++ C0753159_MidTerm_MAD3115S2019/C0753159_MidTerm_MAD3115S2019/BillClasses/IDisplay.swift
@@ -0,0 +1,13 @@
+//
+//  IDisplay.swift
+//  C0753159_MidTerm_MAD3115S2019
+//
+//  Created by User on 2019-07-11.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import Foundation
+protocol IDisplay
+{
+    func display()
+}
