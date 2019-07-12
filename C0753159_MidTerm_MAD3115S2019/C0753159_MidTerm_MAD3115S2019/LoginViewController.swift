02934ed989c84ee1127bc3018eb3e22be2c50ed8
nikhil nikhil <nikhilthakur050@gmail.com>
2019-07-11 20:43:13 -0400

    Login View Controller in Progress

---
+++ C0753159_MidTerm_MAD3115S2019/C0753159_MidTerm_MAD3115S2019/LoginViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  C0753159_MidTerm_MAD3115S2019
+//
+//  Created by User on 2019-07-11.
+//  Copyright © 2019 User. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var Image: UIImageView!
+    
+    @IBOutlet weak var Emailtxt: UITextField!
+    
+    @IBOutlet weak var Passwordtxt: UITextField!
+    
+    @IBOutlet weak var Switch: UISwitch!
+    
+    @IBOutlet weak var RememberMe: UILabel!
+    
+    @IBOutlet weak var Login: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func LoginActionTapped(_ sender: Any) {
+        
+    }
+    
+}
+
